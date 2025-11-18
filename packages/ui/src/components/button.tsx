1a662b16c3667ecd901006691fddfcde297a8080
Rajat Verma <rajat311201@gmail.com>
2025-11-19 01:43:05 +0530

    modify: button and showcase tabs

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -7,7 +7,7 @@ import { cn } from "@repo/ui/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
 
 export const buttonVariants = cva(
-    "inline-flex items-center transition-colors font-medium cursor-pointer justify-center gap-2 whitespace-nowrap rounded-full text-sm  transition-all disabled:pointer-events-none disabled:opacity-50 " +
+    "inline-flex items-center transition-colors font-medium cursor-pointer justify-center gap-2 whitespace-nowrap rounded-lg text-sm  transition-all disabled:pointer-events-none disabled:opacity-50 " +
         " [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]" +
         " aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
     {
@@ -39,11 +39,11 @@ export const buttonVariants = cva(
             },
             size: {
                 default: "h-9 px-4 py-2 has-[>svg]:px-3",
-                sm: "h-8 rounded-full gap-1.5 px-3 has-[>svg]:px-2.5",
-                lg: "h-11 rounded-full px-6 has-[>svg]:px-4",
-                icon: "h-9 w-9 rounded-md",
-                "icon-sm": "h-8 w-8 rounded-md ",
-                "icon-lg": "h-10 w-10 rounded-md ",
+                sm: "h-8 rounded-lg gap-1.5 px-3 has-[>svg]:px-2.5",
+                lg: "h-11 rounded-lg px-6 has-[>svg]:px-4",
+                icon: "h-9 w-9 rounded-lg",
+                "icon-sm": "h-8 w-8 rounded-lg ",
+                "icon-lg": "h-10 w-10 rounded-lg ",
             },
         },
         defaultVariants: {
